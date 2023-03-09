84ac07908b6f38a799c3db115d171c1ab0ba735e
nrubioh <nrubiohoskins@gmail.com>
2023-03-09 19:51:34 -0300

    tree php

--- tree.php
+++ tree.php
@@ -1,5 +1,11 @@
 <?php
 
+$tree = readline("how many lines do you have for the tree size: ");
 
-
+for ($i=0; $i < $tree ; $i++) { 
+    echo "\n";
+    for ($t=0; $t < $i ; $t++) { 
+        echo "*";
+    }
+}
 ?>
