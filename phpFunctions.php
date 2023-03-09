6455f4ce0297d41d503efef4d5461c60f14dff35
nrubioh <nrubiohoskins@gmail.com>
2023-03-09 16:00:16 -0300

    creation of form.html and main.php

--- phpFunctions.php
+++ phpFunctions.php
@@ -2,7 +2,7 @@
 //$name = "Green";
 //$lastname = "Oak";
 function concatenate($name, $lastname){
-echo $name.' '.$lastname;
+echo $name.' '.$lastname."\n";
 }
 concatenate("Green", "Oak"); //to call the fn with the var inside;
 
@@ -11,8 +11,17 @@ function sum($numbers){
     for ($i=0; $i < 4 ; $i++) { 
         $plus = $plus + $numbers[$i];
     }
-    echo "The sum value array is: " .$plus;
+    echo "The sum value array is: " .$plus."\n";
 }
 $numbers = array(1,2,3,4);
 sum($numbers);
+// to know is the fn is or not , BUT IT HAS TO BE DEFINED
+    var_dump(is_array($numbers));
+//to get max and min value of arrays
+    echo max(9,6,8,4,3)."\n";
+    echo min(9,6,8,4,pi())."\n"; //pi()=M_PI
+//to get random number among x-y
+    echo rand(4,8)."\n";
+// squareRoot
+    echo sqrt(4)."\n";
 ?>
