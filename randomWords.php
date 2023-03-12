f220aa1c94aef5c9ad4358b8a930abf9f80f7867
nrubioh <nrubiohoskins@gmail.com>
2023-03-11 21:22:47 -0300

    yayson fix

--- randomWords.php
+++ randomWords.php
@@ -1,10 +1,10 @@
 <?php
 $pkms=array("zapdos","moltres","articuno","lugia","ho-oh");
-    /* $randomWords=array(); */ //Now we are gonna use strings.
-$form="<form action='randomWords2.php'>"; //form action is to link to another form
-for ($i=0; $i < count($pkms) ; $i++){ 
+/* $randomWords=array(); */ //Now we are gonna use strings.
+$form="<form action='randomw2.php'>"; //form action is to link to another form
+for ($i=0; $i < count($pkms); $i++){ 
     $form .= "The pkm: ".str_shuffle($pkms[$i])." ".
-    "<input type='text' name='word.$i'>".
+    "<input type='text' name='word.$i.'>".
     "<br>";
 }
 $button="<button type='submit'>Sent</button>";
