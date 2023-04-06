039e61226e02114ce3af217e0c4c55af6fff3863
nrubioh <nrubiohoskins@gmail.com>
2023-04-06 19:12:53 -0400

    getter and setter

--- POO/index.php
+++ POO/index.php
@@ -2,14 +2,21 @@
 require __DIR__ . '\vendor\autoload.php';
 
 class Post{ //CLASSES
-    public string $id = "1.2.3"; //inside of the class I can to create the VAR / PROPERTY   
+    private string $id = "1.2.3.6256"; //inside of the class I can to create the VAR / PROPERTY   
 
-    public function toSay(){ // also I can to create METHODS
-        return "hi of this post";
+    private function toSay(){ // also I can to create METHODS
+        return "hi of this post with $this->id"; //WITHOUT $$$$$
+    }
+    public function getId():string{
+        return $this->id;
+    }
+    public function setId(string $id){
+        $this->id = $id;
     }
 }
 
 $myObject1 = new Post; //OBJECTS
-
-echo $myObject1->id;
-echo $myObject1->toSay();
+//echo $myObject1->id; WITHOUT $$$$
+    //not work because now is a PRIVATE PROPIERTY (YOU MUST PUT THIS-> TO ACCESS)
+$myObject1->setId('this string is different of GET mettod');
+echo $myObject1->getId();
