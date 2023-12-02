a4789796942c6acc61e023ad675765bf4a148466
nrubioh <nrubiohoskins@gmail.com>
2023-12-01 22:42:32 -0300

    drop table

---
+++ mariadb/12.drop_table.sql
@@ -0,0 +1,12 @@
+USE database_ex1;
+
+DROP TABLE `stations_delete`;
+/* MariaDB [database_ex1]> SHOW TABLES;
++------------------------+
+| Tables_in_database_ex1 |
++------------------------+
+| lines                  |
+| stations               |
+| trains                 | 
++------------------------+ */
+
