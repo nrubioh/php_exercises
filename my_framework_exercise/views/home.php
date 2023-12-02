b204a28da7a130d20b0692c6c14b14f765c1ff85
nrubioh <nrubiohoskins@gmail.com>
2023-12-02 11:40:26 -0300

    My_FW exercise - view folder created / home and layout php

---
+++ my_framework_exercise/views/home.php
@@ -0,0 +1,4 @@
+<div class = "col-8">
+    <h1>Home Page</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos, dolore neque amet hic officiis iure optio eos, dicta quidem, similique asperiores molestiae illum sequi. Quidem porro rem fuga blanditiis accusantium?</p>
+</div>
