8b62c947c1d7f7b72ea5ed6075fc0223388a19cb
nrubioh <nrubiohoskins@gmail.com>
2023-12-01 22:28:12 -0300

    insert sql lines

---
+++ mariadb/4.insert-lines.sql
@@ -0,0 +1,20 @@
+USE database_ex1;
+/* 
+to insert: INSERT INTO `table` (parameter_1,parameter_2,parameter_N) 
+VALUES ("example1","example2"),(...,...);
+to see the parameters: in the workbench : USE database_ex1; SELECT * FROM `lines`;
+to see in the mysql terminal: SELECT * FROM `lines`;
+*/
+INSERT INTO `lines` (name,color) VALUES
+("Line1","Pink"),
+("Line2","Blue"),
+("Line3","Olive Green"),
+("Line4","Cian"),
+("Line5","Yellow"),
+("Line6","Red"),
+("Line7","Orange"),
+("Line8","Green"),
+("Line9","Brown"),
+("LineA","Purple"),
+("LineB","Green and Grey"),
+("Line12","Gold");
